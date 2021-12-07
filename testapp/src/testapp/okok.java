4f86dbb19a1568c88ff4f09ef2eaed5ad2766afc
Teja1004 <tejabattina850@gmail.com>
2021-12-06 20:51:21 -0500

    task 1

---
+++ testapp/src/testapp/okok.java
@@ -0,0 +1,12 @@
+package testapp;
+
+public class okok {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	boolean i=true;
+				boolean b=false;
+				System.out.println(i);
+
+}
+}
